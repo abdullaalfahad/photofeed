ce70341f67cc9bb3105f60a69ecd648b43edae9a
abdullaalfahad <aafahad02@gmail.com>
2025-09-14 19:20:25 +0600

    fix(api/photos): params await in getPhotoById

--- app/api/photos/[id]/route.js
+++ app/api/photos/[id]/route.js
@@ -2,7 +2,7 @@ import { getPhotoById } from "@/lib/image-data";
 import { NextResponse } from "next/server";
 
 export async function GET(_resquest, { params }) {
-  const { id } = params;
+  const { id } = await params;
   const photo = await getPhotoById(id);
 
   return NextResponse.json(photo);
